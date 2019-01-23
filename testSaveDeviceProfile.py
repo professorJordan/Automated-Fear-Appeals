f72c5cb7ebe49cfc7b55b94b460326961a148513
Jordan Shropshire <jordan.shropshire@gmail.com>
2019-01-23 15:52:37 -0600

    Update testSaveDeviceProfile.py

--- testSaveDeviceProfile.py
+++ testSaveDeviceProfile.py
@@ -6,7 +6,7 @@ Created on Wed Jan 23 13:43:35 2019
 """
 import requests
 
-url = 'http://18.219.227.178:5000/deviceprofile'
+url = 'http://...:5000/deviceprofile'
 
 data = {'uid':'X7TWVZHT','file':open('test.txt','rb'),'submit':'Upload'}
 
