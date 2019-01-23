1abdbab2be9fe738ece6d14253f1214e9554f93d
Jordan Shropshire <jordan.shropshire@gmail.com>
2019-01-23 15:52:55 -0600

    Update testRegister.py

--- testRegister.py
+++ testRegister.py
@@ -2,7 +2,7 @@
 
 import requests
 
-url = 'http://18.219.227.178:5000/register'
+url = 'http://...:5000/register'
 data = {'email':'pookey@mittens.com','submit':'Upload'}
 
 r = requests.post(url, data=data)
